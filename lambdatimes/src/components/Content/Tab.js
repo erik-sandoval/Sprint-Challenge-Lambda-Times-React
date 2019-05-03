23223851bf3a5057069b6639899967a479774b6d
erik-sandoval <erik.sandoval@icloud.com>
2019-05-03 10:28:43 -0700

    finished mvp

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const TabSpan = styled.span`
+margin-right: 5px;
+`
 
 const Tab = props => {
+
+
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+
   return (
     <div
+      name={props.tab}
       className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      onClick={props.selectTabHandler}
     >
-      {props.tab.toUpperCase()}
+      <TabSpan name={props.tab}>{props.tab.toUpperCase()}</TabSpan>
     </div>
   );
 };
